01cd4b96ba8033dfca2c85a3ad24ca1e40adc9a0
Steve <steveorsino@gmail.com>
2019-04-14 17:50:53 -0400

    handled some different routes

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
 //middleware
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next();
+app.use('/add-product',(req, res, next) => {
+  console.log('Add product');
+  res.send('<h1>Add Product!<h1>');
 });
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
   console.log('in another middleware');
-  res.send('<h1>hello from express<h1>')
+  res.send('<h1>Hello from express!<h1>')
 });
 
-const server = http.createServer(app);
-
-server.listen(3000)
+app.listen(3000)
