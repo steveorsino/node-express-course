d1f0bf4492031e2893706e2f079607868332a5bf
Steve <steveorsino@gmail.com>
2019-04-15 08:36:09 -0400

    using body-parser, express Router, added routes file structure

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 
 const app = express();
 
 //middleware
-app.use('/add-product',(req, res, next) => {
-  console.log('Add product');
-  res.send('<h1>Add Product!<h1>');
-});
-app.use('/',(req, res, next) => {
-  console.log('in another middleware');
-  res.send('<h1>Hello from express!<h1>')
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
 
 app.listen(3000)
