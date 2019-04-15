936ee8332b08b6784f668c59919d4e3d1d8781cf
Steve <steveorsino@gmail.com>
2019-04-15 17:18:38 -0400

    added public folder and access it statically

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/admin',adminRoutes);
 
