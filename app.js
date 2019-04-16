5d374e91d783e4b11b8993d7cc9de14d55f6c304
Steve <steveorsino@gmail.com>
2019-04-16 15:36:59 -0400

    added products array

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
@@ -12,8 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 
 app.use(shopRoutes);
 
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const rootDir = require('../util/path')
 
 const router = express.Router();
 
+const products = [];
+
 // /admin => GET
 router.get('/add-product',(req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -12,8 +14,10 @@ router.get('/add-product',(req, res, next) => {
 
 // /admin => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
+  products.push( { title: req.body.title } );
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
