d297fe5d28ddccbc74e6d2270cea95920530e0af
steveorsino <steveorsino@yahoo.com>
2019-04-18 18:07:56 -0400

    converted to ejs templating engine

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const path = require('path');
 
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'})); //thats the default but this is a demo
+// app.engine('hbs', expressHbs({
+//   layoutsDir: 'views/layouts',
+//   defaultLayout: 'main-layout', //thats the default but this is a demo
+//   extname: 'hbs'}));
+
 //app.set('view engine', 'pug'); // if using pug
-app.set('view engine', 'hbs')
+
+app.set('view engine', 'hbs'); // if using handlebars
+
+app.set('view engine', 'ejs'); //if using ejs
 app.set('views', 'views'); //not needed beacause the views dir is alredy default. For display of feature if dir changed.
 
 //middleware
@@ -23,7 +30,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page not found'});
+  res.status(404).render('404', {path: '', pageTitle: 'Page not found'});
 });
 
 app.listen(3000)
