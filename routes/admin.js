1c4b4e7ad2fad3882f3023957969b2c9bc187009
steveorsino <steveorsino@yahoo.com>
2019-04-19 13:26:24 -0400

    added products controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path')
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin => GET
-router.get('/add-product',(req, res, next) => {
-  res.render('add-product',{
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin => POST
-router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  products.push( { title: req.body.title } );
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
