8252be4aec68f99d9b63ab4b7b7022f09c88f2f6
steveorsino <steveorsino@yahoo.com>
2019-04-19 16:08:40 -0400

    refactoring

--- models/product.js
+++ models/product.js
@@ -1,41 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'product.json'
+);
+
+const getProductsFromFile = cb => {
+
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([])
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'product.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err)
       });
     });
-  }
+  };
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'product.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([])
-      }
-      console.log(fileContent)
-      cb(JSON.parse(fileContent));
-    })
-  }
+    getProductsFromFile(cb);
+  };
 }
