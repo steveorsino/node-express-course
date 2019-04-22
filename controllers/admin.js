0215ff780db9ce3410594f0f5e7780ea3dcc3d4d
steveorsino <steveorsino@yahoo.com>
2019-04-22 09:04:36 -0400

    added controllers and routes for new views

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,3 @@
-
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -20,13 +19,10 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   Product.fetchAll( products => {
     console.log(products.length)
-    res.render('shop/product-list', {
+    res.render('admin/products', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      pageTitle: 'Admin Products',
+      path: 'admin/products'
     });
   });
 };
